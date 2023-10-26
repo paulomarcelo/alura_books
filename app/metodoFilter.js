d027fa1430bbb16870dd81677c0de7f64a5af4aa
paulomarcelo <paulo.marcelo@gmail.com>
2023-10-26 17:45:27 -0300

    Filtrando livros por categoria no console para os demais botões

--- app/metodoFilter.js
+++ app/metodoFilter.js
@@ -1,7 +1,9 @@
-const btnFiltrarLivrosDeFront = document.getElementById('btnFiltrarLivrosFront')
-btnFiltrarLivrosDeFront.addEventListener('click', filtrarLivrosDeFront)
+const botoes = document.querySelectorAll('.btn')
+botoes.forEach(btn => btn.addEventListener('click', filtrarLivros))
 
-function filtrarLivrosDeFront() {
-    let livrosFiltrados = livros.filter(livro => livro.categoria == 'front-end')
+function filtrarLivros() {
+    const elementoBtn = document.getElementById(this.id)
+    const categoria = elementoBtn.value
+    let livrosFiltrados = livros.filter(livro => livro.categoria == categoria)
     console.table(livrosFiltrados)
 }
